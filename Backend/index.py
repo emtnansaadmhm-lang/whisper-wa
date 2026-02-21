940bc4810e585bef258a56e65d46de4ff6ad1bd4
RAWANALAIDA <rawanalaida2003@gmail.com>
2026-02-22 01:31:38 +0300

    Update index.py

--- Backend/index.py
+++ Backend/index.py
@@ -1,11 +1,10 @@
-# Backend/indexer.py
+# Backend/index.py
 
 def build_index(messages):
     index = {}
 
     for msg_id, text in messages.items():
-        words = text.lower().split()
-        for word in words:
+        for word in text.lower().split():
             if word not in index:
                 index[word] = []
             index[word].append(msg_id)
